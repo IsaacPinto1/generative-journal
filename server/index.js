2f2c94c0232f8367d33b575908c943bd607289fd
IsaacPinto1 <113230969+IsaacPinto1@users.noreply.github.com>
2024-06-27 01:48:38 +0000

    implement API endpoints

--- server/index.js
+++ server/index.js
@@ -2,7 +2,6 @@ const express = require("express")
 const mongoose = require("mongoose")
 const ApiKey = require('./models/ApiKey')
 const dotenv = require('dotenv');
-const cors = require("cors")
 const axios = require('axios');
 const connectDB = require('./config/db');
 
@@ -12,31 +11,46 @@ connectDB();
 
 const app = express()
 app.use(express.json())
-app.use(cors())
+
+app.post('/registerkey', (req, res) =>{ // request, response
+  const {service,apiKey} = req.body
+  ApiKey.create({service, apiKey}).then(objID => res.json({
+    status: "Success",
+    user: objID
+  }))
+  .catch(err => res.json(err))
+})
 
 
 app.get('/api/completion', async (req, res) => {
     try {
-      const apiKeyDoc = await ApiKey.findOne({ service: 'openai' });
+      const apiKeyDoc = await ApiKey.findOne({ service: 'OpenAI' });
       if (!apiKeyDoc) {
         return res.status(404).json({ msg: 'API key not found' });
       }
+      console.log('API Key:', apiKeyDoc.apiKey);
+      console.log('Prompt:', req.body.prompt);
   
       const response = await axios.post(
-        'https://api.openai.com/v1/engines/davinci-codex/completions',
+        'https://api.openai.com/v1/chat/completions',
         {
-          prompt: req.query.prompt,
-          max_tokens: 150,
+          "messages":[{"role":"user", "content":req.body.prompt}],
+          "model": "gpt-3.5-turbo"
         },
         {
           headers: {
             'Content-Type': 'application/json',
-            Authorization: `Bearer ${apiKeyDoc.apiKey}`,
+            'Authorization': `Bearer ${apiKeyDoc.apiKey}`,
           },
         }
       );
-  
-      res.json(response.data.choices[0].text);
+        // Extract the necessary data from the response
+      const completion = response.data.choices[0].message.content;
+
+      console.log(completion)
+      
+      // Send only the extracted data back to the client
+      res.json({ completion });
     } catch (error) {
       console.error('Error fetching data from OpenAI API:', error);
       res.status(500).send('Error fetching data from OpenAI API');
